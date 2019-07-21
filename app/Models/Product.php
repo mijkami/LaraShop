01b66946a06f9438a4c529ee4934fcc9d35db579
mijkami <dimitri.gaillot@gmail.com>
2019-07-21 18:42:33 +0200

    moving Models to /Models folder

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model
+class Product extends Model
 {
     protected $fillable = [
-        'typ', 'dat', 'comment_sor',
+        'typ', 'price', 'image_url',
     ];
 }
