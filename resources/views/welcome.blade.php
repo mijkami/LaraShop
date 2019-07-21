46c1c05275d172388be8c7d4eacc67ea9809148f
mijkami <dimitri.gaillot@gmail.com>
2019-07-21 23:01:17 +0200

    similar views for welcome + products 1/2/3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,9 @@
 
         <h1 class="my-4">Cat Pics Store</h1>
         <div class="list-group">
-          <a href="#" class="list-group-item">Chats mignons</a>
-          <a href="#" class="list-group-item">Chats sportifs</a>
-          <a href="#" class="list-group-item">LOLCATS</a>
+          <a href="/cutecats" class="list-group-item">Chats mignons</a>
+          <a href="/sportscats" class="list-group-item">Chats sportifs</a>
+          <a href="/lolcats" class="list-group-item">LOLCATS</a>
         </div>
 
       </div>
@@ -25,17 +25,19 @@
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-            </div>
-          </div>
+          {{-- <div class="carousel-inner" role="listbox"> --}}
+          <?php
+            // $x=0;
+            // foreach ($product as $pro){
+            //     $x=$x+1;
+            //     echo '<div class="carousel-item';
+            //     if ($x==1){echo ' active';}
+            //     echo '">';
+            //     echo '<img class="d-block img-fluid" src="'.$pro->image_url.'" >';
+            //     echo '</div>';
+            // }
+          ?>
+         {{-- </div>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
@@ -44,106 +46,33 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
-        </div>
+        </div> --}}
 
         <div class="row">
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item One</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Two</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Three</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Four</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Five</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Six</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
-
+        <?php
+        foreach ($product as $pro){
+            echo '<div class="col-lg-4 col-md-6 mb-4">';
+            echo '<div class="card h-100">';
+            echo '<a href="#"><img class="card-img-top" src="'.$pro->image_url.'" alt=""></a>';
+            echo '<div class="card-body">';
+            echo '<h4 class="card-title">';
+            echo '<a href="#">'.$pro->price.' euros</a>';
+            echo '</h4>';
+            echo '<h5>'.$pro->name.'</h5>';
+            echo '<p class="card-text">Type : '.$pro->typ.'<br>Commentaire : '.$pro->comment.'</p>';
+            echo '</div>';
+            echo '<div class="card-footer">';
+                if ($pro->stars==1){$a="&#9733; &#9734; &#9734; &#9734; &#9734;";}
+                elseif($pro->stars==2){$a="&#9733; &#9733; &#9734; &#9734; &#9734;";}
+                elseif($pro->stars==3){$a="&#9733; &#9733; &#9733; &#9734; &#9734;";}
+                elseif($pro->stars==4){$a="&#9733; &#9733; &#9733; &#9733; &#9734;";}
+                elseif($pro->stars==5){$a="&#9733; &#9733; &#9733; &#9733; &#9733;";}
+            echo '<small class="text-muted">'.$a.'</small>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
         </div>
         <!-- /.row -->
 
