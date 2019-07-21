6f6465aca4ea62407350d39db59e95bb26ecfbbf
mijkami <dimitri.gaillot@gmail.com>
2019-07-21 18:43:30 +0200

    adding  Product content to seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,22 +1,45 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Cart;
-use App\Products;
-use Carbon\Carbon;
+use App\Models\Product;
+// use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return voideb
-     */
     public function run()
     {
         DB::table('products')->delete();
-        $array = array("chats mignons", "chats sportifs", "LOLCATS");
+        // $array = array("chats mignons", "chats sportifs", "LOLCATS");
+
         // initialisation de faker
         $faker = Faker\Factory::create('fr_FR');
+
+
+        for ($i = 0; $i < 5; $i++) {
+            $product = new Product;
+            $product->name = $faker->lastName;
+            $product->typ = 'chats mignons';
+            $product->price = rand(12, 35);
+            $product->image_url = $faker->image('images',700,400, 'cats');
+            $product->save();
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $product = new Product;
+            $product->name = $faker->lastName;
+            $product->typ = 'chats sportifs';
+            $product->price = rand(27, 49);
+            $product->image_url = $faker->image('images', 700, 400, 'cats');
+            $product->save();
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $product = new Product;
+            $product->name = $faker->lastName;
+            $product->typ = 'LOLCATS';
+            $product->price = rand(275, 499);
+            $product->image_url = $faker->image('images', 700, 400, 'cats');
+            $product->save();
+        }
     }
 }
