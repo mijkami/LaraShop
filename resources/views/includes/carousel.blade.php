8a4d8014afeedeb029a0a604ce40cfa49373a423
mijkami <dimitri.gaillot@gmail.com>
2019-07-26 09:23:10 +0200

    adding carousel include page

---
+++ resources/views/includes/carousel.blade.php
@@ -0,0 +1,73 @@
+<section class="hero-section mt-n5 mb-5">
+    <div class="hero-slider owl-carousel">
+        <div class="hs-item set-bg" data-setbg="images/carousel/bg1.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-7 text-white">
+                        <span>Cute Cats</span>
+                        <h2>All the fluffs</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum sus-pendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                        <a href="#" class="site-btn sb-line">DISCOVER</a>
+                        <a href="#" class="site-btn sb-white">ADD TO CART</a>
+                    </div>
+                </div>
+                <div class="offer-card text-white">
+                    <span>from</span>
+                    <h2>$23</h2>
+                    <p>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+        <div class="hs-item set-bg" data-setbg="images/carousel/bg-2.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-7 text-white">
+                        <span>Dynamic Cats</span>
+                        <h2>All the bounces</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum sus-pendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                        <a href="#" class="site-btn sb-line">DISCOVER</a>
+                        <a href="#" class="site-btn sb-white">ADD TO CART</a>
+                    </div>
+                </div>
+                <div class="offer-card text-white">
+                    <span>from</span>
+                    <h2>$27</h2>
+                    <p>SHOP NOW</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="slide-num-holder" id="snh-1"></div>
+    </div>
+
+
+    {{-- <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol> --}}
+          {{-- <div class="carousel-inner" role="listbox"> --}}
+          <?php
+            // $x=0;
+            // foreach ($product as $pro){
+            //     $x=$x+1;
+            //     echo '<div class="carousel-item';
+            //     if ($x==1){echo ' active';}
+            //     echo '">';
+            //     echo '<img class="d-block img-fluid" src="'.$pro->image_url.'" >';
+            //     echo '</div>';
+            // }
+          ?>
+         {{-- </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div> --}}
+</section>
