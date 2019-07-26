d9133f0e64a6cf4804da664b0e8a3a68fdef735a
mijkami <dimitri.gaillot@gmail.com>
2019-07-26 09:23:21 +0200

    moving carousel page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+@include('includes.carousel')
 <div class="container">
 
     <div class="row">
       <!-- /.col-lg-3 -->
 
       <div class="col-lg-12">
-
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          {{-- <div class="carousel-inner" role="listbox"> --}}
-          <?php
-            // $x=0;
-            // foreach ($product as $pro){
-            //     $x=$x+1;
-            //     echo '<div class="carousel-item';
-            //     if ($x==1){echo ' active';}
-            //     echo '">';
-            //     echo '<img class="d-block img-fluid" src="'.$pro->image_url.'" >';
-            //     echo '</div>';
-            // }
-          ?>
-         {{-- </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div> --}}
 
         <div class="row">
         <?php
