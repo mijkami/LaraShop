a3f014ade8ba99f586a2ee74697baec44284d883
mijkami <dimitri.gaillot@gmail.com>
2019-07-26 09:37:46 +0200

    moving in-product page carousel draft in /= page

---
+++ resources/views/pages/carouselproduct.blade.php
@@ -0,0 +1,22 @@
+{{-- <div class="carousel-inner" role="listbox"> --}}
+          <?php
+            // $x=0;
+            // foreach ($product as $pro){
+            //     $x=$x+1;
+            //     echo '<div class="carousel-item';
+            //     if ($x==1){echo ' active';}
+            //     echo '">';
+            //     echo '<img class="d-block img-fluid" src="'.$pro->image_url.'" >';
+            //     echo '</div>';
+            // }
+          ?>
+         {{-- </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div> --}}
