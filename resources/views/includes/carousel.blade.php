8f83bf3e8dc29fa614aaf0c99da96d4fdb825f98
mijkami <dimitri.gaillot@gmail.com>
2019-07-31 10:52:30 +0200

    moving carrousel css / js dependencies

--- resources/views/includes/carousel.blade.php
+++ resources/views/includes/carousel.blade.php
@@ -1,4 +1,13 @@
-<section class="hero-section mt-n5 mb-5">
+<script src="js/jquery-3.2.1.min.js"></script>
+{{-- <script src="js/bootstrap.min.js"></script> --}}
+<script src="js/jquery.slicknav.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/jquery.zoom.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+
+
+<section class="hero-section mb-5">
     <div class="hero-slider owl-carousel">
         <div class="hs-item set-bg" data-setbg="images/carousel/bg2.jpg">
             <div class="container">
---
+++ resources/views/includes/scriptscaroussel.blade.php
@@ -0,0 +1,9 @@
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.slicknav.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/jquery.zoom.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+{{-- <script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
